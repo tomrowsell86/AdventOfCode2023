eb02f931f772a7132908584fd096e538b50ba829
Tom Rowsell <rowselltom@gmail.com>
2023-12-05 13:41:10 +0000

    Uncommented part a and refactored day01

--- Day01/index.js
+++ Day01/index.js
@@ -20,58 +20,55 @@ fs.readFile("input.txt", "utf-8", (_, f) => {
         return { value: Number.parseInt(c, 10), position: i };
       })
       .filter((n) => !Number.isNaN(n.value));
-  // const partA = f
-  //   .split("\n")
-  //   .map((l) =>
-  //     numberIndexes(l)
-  //       .filter((_, i, { length }) => i === 0 || i === length - 1)
-  //       .reduce(
-  //         (s, curr, i, { length }) =>
-  //           s +
-  //           (length < 2 ? curr.value : 0) +
-  //           (i === 0 ? curr.value * 10 : curr.value),
-  //         0
-  //       )
-  //   )
-  //   .map((c) => {
-  //     return c;
-  //   })
-  //   .reduce((t, c) => t + c, 0);
-  // console.log(`Part A ${partA}`);
+  const partA = f
+    .split("\n")
+    .map((l) =>
+      numberIndexes(l)
+        .filter((_, i, { length }) => i === 0 || i === length - 1)
+        .reduce(
+          (s, curr, i, { length }) =>
+            s +
+            (length < 2 ? curr.value : 0) +
+            (i === 0 ? curr.value * 10 : curr.value),
+          0
+        )
+    )
+    .map((c) => {
+      return c;
+    })
+    .reduce((t, c) => t + c, 0);
+  console.log(`Part A : ${partA}`);
 
   const partB = f
     .split("\n")
     .map((l) => {
       const numbers = numberIndexes(l);
-      const nums = Array.from(numberMap.keys())
-        .map((n) => [n,numberMap.get(n)])
-        .map(([ntext,n]) => {
-          let indexN =  l.indexOf(ntext);
-          let matches = []
-          while(indexN !== -1){
-           matches.push({position : indexN, value: n})
-           indexN = l.indexOf(ntext,indexN + 1)
+      return Array.from(numberMap.keys())
+        .map((n) => [n, numberMap.get(n)])
+        .map(([ntext, n]) => {
+          let indexN = l.indexOf(ntext);
+          let matches = [];
+          while (indexN !== -1) {
+            matches.push({ position: indexN, value: n });
+            indexN = l.indexOf(ntext, indexN + 1);
           }
           return matches;
-
         })
         .filter(({ length }) => length >= 0)
         .flat()
         .concat(numbers)
-        
         .sort((a, b) =>
           a.position < b.position ? -1 : a.position > b.position ? 1 : 0
         )
         .reduce(
           (total, curr, i, { length }) =>
             (i === 0 ? curr.value * 10 : i === length - 1 ? curr.value : 0) +
-            (length === 1 ? curr.value : 0) + total
-            
-        ,0);
-        console.log(nums)
-        return nums
+            (length === 1 ? curr.value : 0) +
+            total,
+          0
+        );
     })
     .reduce((total, c) => total + c, 0);
 
-  console.log(partB);
+  console.log(`Part B : ${partB}`);
 });
